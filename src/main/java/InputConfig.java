38a35d9990eee314274b1e92094f38f69af82154
Edwin Morales Jr <150313733+EdwinOMG@users.noreply.github.com>
2025-02-13 23:16:49 -0500

    Created Process API (#25)
    * Created Process API
    
    Process api between data store and computer engine
    
    * Fixed error message

---
+++ src/main/java/InputConfig.java
@@ -0,0 +1,5 @@
+package main.java;
+
+public interface InputConfig {
+// need to implement
+}
---
+++ src/main/java/PrototypeDataStore.java
@@ -0,0 +1,32 @@
+package main.java;
+
+import main.java.project.annotations.ProcessAPIPrototype;
+
+public class PrototypeDataStore {
+	// datastore is the api
+	@ProcessAPIPrototype
+	public void prototype(DataStore dataStore) {
+
+		InputConfig inputConfig = new InputConfig() {
+			
+		};
+		
+		OutputConfig outputConfig = new OutputConfig() {
+			
+		};
+	
+		// reads in data
+		Iterable<Integer> readData = dataStore.read(inputConfig);
+			
+		//do something with readdata 
+		
+		// takes in where the data will go, and integers previously read, 
+		//stores the data at the output, and passes on a object to indicate if it was successful
+		OutputResult outputResult = dataStore.appendResult(outputConfig, readData);
+
+		//if it fails it will print damn...
+		if (outputResult.getStatus() != OutputResult.ShowResultStatus.SUCCESS) {
+			System.out.println("FAILED");
+		}
+}
+}

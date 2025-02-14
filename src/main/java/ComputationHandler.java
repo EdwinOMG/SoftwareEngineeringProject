d675e1f7b0e51d39c697758f6d49da4251d90634
Kevin P <portillk3@newpaltz.edu>
2025-02-13 22:12:30 -0500

    Created NetworkAPI

---
+++ src/main/java/ComputationHandler.java
@@ -0,0 +1,14 @@
+package main.java;
+
+import main.java.project.annotations.NetworkAPI;
+
+/** - A network API that facilitates communication between the user and compute engine.
+    - This design will use a single request wrapper to encapsulate all required user inputs.
+    - Also, individual parameters could be specified, by using method overloading to handle the default delimiter. 
+    - This method is similar to Builder pattern. **/
+
+@NetworkAPI 
+public interface ComputationHandler {
+	
+    ComputeResult compute(ComputeRequest request);
+}

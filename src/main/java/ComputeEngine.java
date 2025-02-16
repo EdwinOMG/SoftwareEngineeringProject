481e259d5424f1403786afc38689c330ef750b8b
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2025-02-15 21:00:11 -0800

    Update ComputeEngine.java

--- src/main/java/ComputeEngine.java
+++ src/main/java/ComputeEngine.java
@@ -2,9 +2,3 @@
 public interface ComputeEngine { 
   String compute(int value);                              
 } 
-public class ComputeEnginePrototype { 
-  @ConceptualAPIPrototype
-  public void prototype(ComputeEngine engine) { 
-    String result = engine.compute(1); 
-  } 
-} 

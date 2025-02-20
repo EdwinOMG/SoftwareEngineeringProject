032c292709b1ddfd51f72015eed9a270f38d8aaf
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2025-02-19 18:36:27 -0800

    Smoke Tests

---
+++ test/project/annotations/SmokeTest.java
@@ -0,0 +1,11 @@
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+public class SmokeTest{
+  public void testNetwork(){
+  }
+  public void testProcess()[
+  }
+  public void testConceptual(){
+  }
+}

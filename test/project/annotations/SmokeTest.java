9dbb1b984e78960061c5b24ff75347f738e9897a
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2025-02-20 08:39:26 -0800

    Update SmokeTest.java

--- test/project/annotations/SmokeTest.java
+++ test/project/annotations/SmokeTest.java
@@ -19,14 +19,14 @@ public class SmokeTest{
     OutputConfig output="5,25,29,85,89,145,42, 20,4,16,37,58,89";
     x.read(input);
     x.appendResult(output, input);
-    Assert.assertionEquals(x.appendResult.getStatus(SUCCESS),  x.appendResult(output, input););
+    Assert.assertionEquals(x.appendResult.getStatus(SUCCESS),  x.appendResult(output, input));
     
   }
   public void testConceptual(){
     ComputeEngine x=Mockito.mock(x.class);
     int y=5;
     x.compute(y);
-    String s="five"
+    String s="five";
     Assert.assertionEquals(s, x.compute(y));
    
   }

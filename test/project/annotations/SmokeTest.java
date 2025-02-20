a086f8974489136c5e9129e5cd903b30b9436b2c
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2025-02-19 18:46:48 -0800

    Update SmokeTest.java

--- test/project/annotations/SmokeTest.java
+++ test/project/annotations/SmokeTest.java
@@ -1,12 +1,17 @@
 import org.mockito.Mockito;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
 public class SmokeTest{
   @Test
   public void testNetwork(){
+    ComputationHandler x=Mockito.mock(x.class);
+    
   }
-  public void testProcess()[
+  public void testProcess(){
+    DataStore x=Mockito.mock(x.class);
   }
   public void testConceptual(){
+    ComputeEngine x=Mockito.mock(x.class);
   }
 }

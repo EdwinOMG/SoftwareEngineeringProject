aeb1e7531df38be5cb40e732fa1bdf95fd6d6da2
Kevin Portillo <portillk3@newpaltz.edu>
2025-02-20 22:42:14 -0500

    assigment pt 4a submission (#39)
    * assigment pt 4a submission
    
    Added TestInputConfig and TestOutputConfig classes to test folder
    
    * fixes to pass gradlecheck
    
    removed @Override from Input and output methods

---
+++ test/TestOutputConfig.java
@@ -0,0 +1,21 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.OutputConfig;
+
+public class TestOutputConfig implements OutputConfig {
+    private final List<String> outputValues = new ArrayList<>();
+
+    
+    //create writeOutput method in OutputConfig interface
+    public void writeOutput(String output) {
+        outputValues.add(output);
+    }
+
+    public List<String> getOutput() {
+        return outputValues;
+    }
+}
+

2ccc67d51fd74f115c5b4bf416c3e9a59a8a8026
Kevin P <portillk3@newpaltz.edu>
2025-02-21 01:29:13 -0500

    Create TestIntegDataStore.java

---
+++ test/project/annotations/TestIntegDataStore.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.DataStore;
+import main.java.InputConfig;
+import main.java.OutputConfig;
+import main.java.OutputResult;
+
+public class TestIntegDataStore implements DataStore {
+
+    @Override
+    public Iterable<Integer> read(InputConfig inputConfig) {
+        return inputConfig.getInput(); // Get the input from the TestInputConfig
+    }
+
+    @Override
+    public OutputResult appendResult(OutputConfig outputConfig, Iterable<Integer> readData) {
+        List<Integer> processedResults = new ArrayList<>();
+
+        // Process the integers and write to outputConfig using writeOutput(Integer)
+        for (Integer number : readData) {
+            Integer result = number + 1; // Simulating processing (e.g., incrementing by 1)
+            processedResults.add(result);
+            outputConfig.writeOutput(result); // Write Integer output to TestOutputConfig
+        }
+
+        // Return status directly without creating a separate class
+        return new OutputResult() {
+            @Override
+            public ShowResultStatus getStatus() {
+                return ShowResultStatus.SUCCESS; // Assume success for this test case
+            }
+        };
+    }
+}

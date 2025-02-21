147e396a8c19bc728e9e6407f67bd46a763788cd
gavinbell123 <157721855+gavinbell123@users.noreply.github.com>
2025-02-20 18:27:57 -0800

    Create TestComputeEngine.java

---
+++ TestComputeEngine.java
@@ -0,0 +1,33 @@
+package project.annotations;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import main.java.ComputeEngine;
+import main.java.ComputeEngineImpl;
+
+public class TestComputeEngine {
+    private ComputeEngineImpl computeEngine;
+
+    @Before
+    public void setUp() {
+        computeEngine = new ComputeEngineImpl();
+    }
+    @Test
+    public void testComputeEngineExists() {
+        assertNotNull(computeEngine); // make sure it exist
+    }
+
+    @Test
+    public void testComputeMethodReturnsListOfN() {
+        int input = 5;
+
+        Iterable<Integer> result = computeEngine.compute(input);
+        assertNotNull("Compute method should be a list of the input");
+
+    }
+
+}

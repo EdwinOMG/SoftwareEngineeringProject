fe5cc0ee340f99c106767865c4678402942edc8f
Kevin P <portillk3@newpaltz.edu>
2025-02-27 19:17:13 -0500

    Removed @Override on line 7

--- src/main/java/ComputeEngineImpl.java
+++ src/main/java/ComputeEngineImpl.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class ComputeEngineImpl  implements ComputeEngine{
 
-	@Override
+	
 	public Iterable<Integer> compute(int i){
 		return List.of(i); //List of just i
 	}

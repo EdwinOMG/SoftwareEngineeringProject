68542ebabe3a7fee99a669b35851e6d7bdef04df
Edwin Morales Jr <150313733+EdwinOMG@users.noreply.github.com>
2025-02-28 18:58:09 -0500

    Changed alot to make digit chains class wrapper work (#60)

--- src/main/java/ComputeEngineImpl.java
+++ src/main/java/ComputeEngineImpl.java
@@ -1,38 +1,37 @@
 package main.java;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ComputeEngineImpl  implements ComputeEngine{
-	
-	
-	// method to square each digit then return the sum to the compute() method
-	private int squareDigitSum(int i) {
-		int sum = 0;
-		while(i > 0) {
-			int digit = i % 10; // get last digit
-			sum += (digit * digit); //square digit and add it 
-			i = i / 10; // get rid of digit
-		}
-		return sum;
-	}
-	
-	// This is where square digit chain algorithm will be.
-	@Override
-	public Iterable<Integer> compute(int num){
-		// create Iterable<Integer>
-		ArrayList<Integer> digitChain = new ArrayList<>();
-		digitChain.add(num);
-		while(num != 89 && num != 1){
-			//method that returns sum of squared digits
-			num = squareDigitSum(num);
-			//add total sum to ArrayList<Integer>
-			digitChain.add(num);
-		}
-		return digitChain;
+public class ComputeEngineImpl implements ComputeEngine {
 
-	}
-	
-	
+    private int squareDigitSum(int i) {
+        int sum = 0;
+        while (i > 0) {
+            int digit = i % 10;
+            sum += (digit * digit);
+            i = i / 10;
+        }
+        return sum;
+    }
 
-	
+    private List<Integer> computeChain(int num) {
+        List<Integer> chain = new ArrayList<>();
+        chain.add(num);
+        while (num != 89 && num != 1) {
+            num = squareDigitSum(num);
+            chain.add(num);
+        }
+        return chain;
+    }
+
+    @Override
+    public DigitChains compute(Iterable<Integer> inputList) {
+        List<Iterable<Integer>> result = new ArrayList<>();
+        for (int num : inputList) {
+            List<Integer> chain = computeChain(num);
+            result.add(chain);
+        }
+        return new DigitChains(result);
+    }
 }

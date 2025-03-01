c7e1b0635f0daa98c9a3fb8a538969d2514b54db
Kevin Portillo <portillk3@newpaltz.edu>
2025-02-28 23:14:38 -0500

    Changes to ComputationHandlerImpl and DataStoreImpl after Edwins pt 1 addition (#61)

--- src/main/java/ComputationHandlerImpl.java
+++ src/main/java/ComputationHandlerImpl.java
@@ -22,4 +22,5 @@ public class ComputationHandlerImpl implements ComputationHandler {
                 ? ComputeEngineResult.SUCCESS
                 : ComputeEngineResult.FAIL;
     }
-}
+}  
+//delete this later

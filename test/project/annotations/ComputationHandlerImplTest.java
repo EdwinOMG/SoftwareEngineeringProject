e7b157d7a8f55fab1f66891ef993cd7ac7fd0f62
Kevin P <portillk3@newpaltz.edu>
2025-03-07 21:09:02 -0500

    Update ComputationHandlerImplTest.java

--- test/project/annotations/ComputationHandlerImplTest.java
+++ test/project/annotations/ComputationHandlerImplTest.java
@@ -1,38 +1,39 @@
-package test.project.annotations;
+package project.annotations;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
-import org.junit.Before;
+import org.mockito.Mockito;
 
-import src.main.java.ComputationHandlerImpl;
-import src.main.java.ComputeEngine;
-import src.main.java.ComputeEngineRequest;
-import src.main.java.ComputeEngineResult;
-import src.main.java.DataStore;
+import main.java.ComputationHandlerImpl;
+import main.java.ComputeEngine;
+import main.java.ComputeEngineRequest;
+import main.java.ComputeEngineResult;
+import main.java.DataStore;
+
+
+
 
 public class ComputationHandlerImplTest {
 
-	 @Test
-	    public void constructorShouldThrowExceptionWhenDependenciesAreNull() {
-	        ComputeEngine mockEngine = Mockito.mock(ComputeEngine.class);
-	        DataStore mockDataStore = Mockito.mock(DataStore.class);
-	        
-	        assertThrows(IllegalArgumentException.class, () -> new ComputationHandlerImpl(null, mockDataStore));
-	        assertThrows(IllegalArgumentException.class, () -> new ComputationHandlerImpl(mockEngine, null));
-	        assertThrows(IllegalArgumentException.class, () -> new ComputationHandlerImpl(null, null));
-	    }
+     @Test
+        public void constructorShouldThrowExceptionWhenDependenciesAreNull() {
+            ComputeEngine mockEngine = Mockito.mock(ComputeEngine.class);
+            DataStore mockDataStore = Mockito.mock(DataStore.class);
 
-	    @Test
-	    public void computeShouldFailForInvalidRequest() {
-	        ComputeEngine mockEngine = Mockito.mock(ComputeEngine.class);
-	        DataStore mockDataStore = Mockito.mock(DataStore.class);
-	        ComputationHandlerImpl handler = new ComputationHandlerImpl(mockEngine, mockDataStore);
-	        
-	        ComputeEngineRequest invalidRequest = new ComputeEngineRequest(null, null, ',');
-	        assertEquals(ComputeEngineResult.FAIL, handler.compute(invalidRequest));
-	    }
-	}
+            assertThrows(IllegalArgumentException.class, () -> new ComputationHandlerImpl(null, mockDataStore));
+            assertThrows(IllegalArgumentException.class, () -> new ComputationHandlerImpl(mockEngine, null));
+            assertThrows(IllegalArgumentException.class, () -> new ComputationHandlerImpl(null, null));
+        }
 
+        @Test
+        public void computeShouldFailForInvalidRequest() {
+            ComputeEngine mockEngine = Mockito.mock(ComputeEngine.class);
+            DataStore mockDataStore = Mockito.mock(DataStore.class);
+            ComputationHandlerImpl handler = new ComputationHandlerImpl(mockEngine, mockDataStore);
+
+            ComputeEngineRequest invalidRequest = new ComputeEngineRequest(null, null, ',');
+            assertEquals(ComputeEngineResult.FAIL, handler.compute(invalidRequest));
+        }
+    }

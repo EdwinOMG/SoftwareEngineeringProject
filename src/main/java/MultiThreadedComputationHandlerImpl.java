ac03a463814fbf7cdc19ae72b8587c15cf220381
Kevin P <portillk3@newpaltz.edu>
2025-04-03 20:14:26 -0400

    Update MultiThreadedComputationHandlerImpl.java

--- src/main/java/MultiThreadedComputationHandlerImpl.java
+++ src/main/java/MultiThreadedComputationHandlerImpl.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.Callable;
-import src.main.java.ComputeEngineResult;
+
 
 public class MultiThreadedComputationHandlerImpl implements ComputationHandler {
     private final ComputeEngine computeEngine;
